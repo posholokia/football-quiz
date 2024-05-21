877c3ebc404d2ef0f232b82d0d2dec30ce5dd6da
posholokia <ilya.posholokk@gmail.com>
2024-05-22 01:03:17 +0600

    fix: healthcheck

--- app/healthcheck/api.py
+++ app/healthcheck/api.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter
 
 from starlette import status
 
@@ -6,7 +6,7 @@ from starlette import status
 router = APIRouter()
 
 
-@router.post("/healthcheck/", status_code=status.HTTP_200_OK)
+@router.get("/healthcheck/", status_code=status.HTTP_200_OK)
 async def health_check() -> None:
     """Сервисное API, проверка, что контейнер жив"""
     return None
