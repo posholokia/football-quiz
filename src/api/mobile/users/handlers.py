15f993dce0a37aecca73495b776d4105730523cd
posholokia <ilya.posholokk@gmail.com>
2024-07-30 14:50:46 +0600

    fix: ladder schema

--- src/api/mobile/users/handlers.py
+++ src/api/mobile/users/handlers.py
@@ -40,6 +40,7 @@ from services.security.mobile_auth import (
 from ..utils import get_offset
 from .schema import (
     ApiKeySchema,
+    LadderStatisticRetrieveSchema,
     ProfileSchema,
     StatisticsRetrieveSchema,
     StatisticsUpdateSchema,
@@ -160,7 +161,7 @@ async def get_ladder_profile(
     cred: MobileAuthorizationCredentials = Depends(http_device),
     model: Type[Base] = Depends(get_statistic_model),
     container: Container = Depends(get_container),
-) -> PaginationResponseSchema[StatisticsRetrieveSchema]:
+) -> PaginationResponseSchema[LadderStatisticRetrieveSchema]:
     permissions: ProfilePermissions = container.resolve(ProfilePermissions)
     await permissions.has_permission(pk, cred.token)
 
@@ -174,7 +175,7 @@ async def get_ladder_profile(
     paginator: LimitOffsetPaginator = container.resolve(
         service_key=LimitOffsetPaginator,
         pagination=pagination_in,
-        schema=StatisticsRetrieveSchema,
+        schema=LadderStatisticRetrieveSchema,
     )
 
     res = await paginator.paginate(action.get_top_ladder)
@@ -200,7 +201,7 @@ async def get_ladder(
     cred: MobileAuthorizationCredentials = Depends(http_device),
     model: Type[Base] = Depends(get_statistic_model),
     container: Container = Depends(get_container),
-) -> PaginationResponseSchema[StatisticsRetrieveSchema]:
+) -> PaginationResponseSchema[LadderStatisticRetrieveSchema]:
     permissions: DevicePermissions = container.resolve(DevicePermissions)
     await permissions.has_permission(cred.token)
 
@@ -211,7 +212,7 @@ async def get_ladder(
     paginator: LimitOffsetPaginator = container.resolve(
         service_key=LimitOffsetPaginator,
         pagination=pagination_in,
-        schema=StatisticsRetrieveSchema,
+        schema=LadderStatisticRetrieveSchema,
     )
     res = await paginator.paginate(action.get_top_ladder)
 
