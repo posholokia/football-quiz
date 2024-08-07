a80c3bcdc365da67bc8641e51f3959eb4eb66f59
posholokia <ilya.posholokk@gmail.com>
2024-08-07 23:37:02 +0600

    feat: create question

--- src/apps/quiz/exceptions/complaint.py
+++ src/apps/quiz/exceptions/complaint.py
@@ -4,12 +4,6 @@ from core.constructor.exceptions import BaseHTTPException
 
 
 @dataclass(eq=False)
-class QuestionDoesNotExists(BaseHTTPException):
-    code: int = 400
-    detail: str = "Такой вопрос не существует"
-
-
-@dataclass(eq=False)
 class CategoryComplaintDoesNotExists(BaseHTTPException):
     code: int = 400
     detail: str = "Такой категории не существует"
