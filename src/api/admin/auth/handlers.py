28e64a41dc7aee88bf20f65974113685bb4862c7
posholokia <ilya.posholokk@gmail.com>
2024-08-08 22:04:51 +0600

    fix: jwt error handler

--- src/api/admin/auth/handlers.py
+++ src/api/admin/auth/handlers.py
@@ -13,7 +13,6 @@ from fastapi import (
 from starlette import status
 
 from apps.users.actions.user import AdminAuthAction
-from apps.users.services.auth.jwt_auth.models import BlacklistRefreshToken
 from config.containers import get_container
 
 
@@ -30,16 +29,10 @@ async def login(
     container: Container = Depends(get_container),
 ) -> TokenObtainPairSchemaSchema:
     action: AdminAuthAction = container.resolve(AdminAuthAction)
-    user = await action.login(
+    refresh, access = await action.login(
         username=credentials.username,
         password=credentials.password,
     )
-    token_service: BlacklistRefreshToken = container.resolve(
-        BlacklistRefreshToken
-    )
-    refresh = await token_service.for_user(user)
-    access = await token_service.access_token(refresh)
-
     return TokenObtainPairSchemaSchema(
         access=access,
         refresh=refresh,
@@ -55,11 +48,8 @@ async def refresh_token(
     token: RefreshTokenSchema,
     container: Container = Depends(get_container),
 ) -> AccessTokenSchema:
-    token_service: BlacklistRefreshToken = container.resolve(
-        BlacklistRefreshToken
-    )
-    access = await token_service.access_token(token.refresh)
-
+    action: AdminAuthAction = container.resolve(AdminAuthAction)
+    access = await action.refresh_token(token.refresh)
     return AccessTokenSchema(
         access=access,
     )
@@ -74,9 +64,6 @@ async def blacklist_token(
     token: RefreshTokenSchema,
     container: Container = Depends(get_container),
 ) -> None:
-    token_service: BlacklistRefreshToken = container.resolve(
-        BlacklistRefreshToken
-    )
-    await token_service.set_blacklist(token.refresh)
-
+    action: AdminAuthAction = container.resolve(AdminAuthAction)
+    await action.set_blacklist_token(token.refresh)
     return
--- src/apps/users/actions/user.py
+++ src/apps/users/actions/user.py
@@ -1,22 +1,57 @@
 from dataclasses import dataclass
 
-from apps.users.exceptions.auth import InvalidAuthCredentials
-from apps.users.models import UserEntity
+from apps.users.exceptions.auth import (
+    InvalidAuthCredentials,
+    InvalidToken,
+)
 from apps.users.permissions.admin import IsAdminUser
+from apps.users.services.auth.jwt_auth.models import BlacklistRefreshToken
 from apps.users.services.auth.pwd_hash import check_password
 from apps.users.services.storage.base import IUserService
+from services.jwt_token.exceptions import (
+    DecodeJWTError,
+    InvalidTokenType,
+    TokenInBlacklistError,
+)
 
 
 @dataclass
 class AdminAuthAction:
     repository: IUserService
     permission: IsAdminUser
+    token_service: BlacklistRefreshToken
 
-    async def login(self, username: str, password: str) -> UserEntity:
+    async def login(self, username: str, password: str) -> tuple[str, str]:
         user = await self.repository.get_by_username(username)
 
         if user is None or not check_password(password, user.password):
             raise InvalidAuthCredentials()
 
         await self.permission.has_permission(user)
-        return user
+
+        refresh = await self.token_service.for_user(user)
+        access = await self.token_service.access_token(refresh)
+        return refresh, access
+
+    async def refresh_token(self, token: str) -> str:
+        try:
+            access = await self.token_service.access_token(token)
+        except DecodeJWTError:
+            raise InvalidToken(
+                detail="Некорректный токен, ошибка при попытке декодировать"
+            )
+        except InvalidTokenType:
+            raise InvalidToken(detail="Некорректный тип токена")
+        except TokenInBlacklistError:
+            raise InvalidToken(detail="Токен в черном списке")
+        return access
+
+    async def set_blacklist_token(self, token: str) -> None:
+        try:
+            await self.token_service.set_blacklist(token)
+        except DecodeJWTError:
+            raise InvalidToken(
+                detail="Некорректный токен, ошибка при попытке декодировать"
+            )
+        except InvalidTokenType:
+            raise InvalidToken(detail="Некорректный тип токена")
