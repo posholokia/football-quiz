890cb35e0f509c96db18d17bdd4dbaf95f2e329e
posholokia <ilya.posholokk@gmail.com>
2024-08-10 22:27:37 +0600

    feat: refactoring

--- src/apps/users/services/storage/sqla/titles.py
+++ src/apps/users/services/storage/sqla/titles.py
@@ -4,6 +4,10 @@ from sqlalchemy import (
     select,
     update,
 )
+from sqlalchemy.exc import (
+    IntegrityError,
+    NoResultFound,
+)
 
 from apps.users.models import BestPlayerTitle
 from apps.users.services.storage.base import IProfileTitleService
@@ -18,26 +22,32 @@ class ORMProfileTitleService(IProfileTitleService):
         self,
         profile_pk: int,
     ) -> BestPlayerTitle:
-        """Вызывается внутри другой сессии, не напрямую"""
         async with self.db.get_session() as session:
-            query = select(BestPlayerTitle).where(
-                BestPlayerTitle.profile_id == profile_pk
-            )
-            res = await session.execute(query)
-            title = res.scalar()
-
-            if title is None:  # если не найдена
+            try:
+                query = select(BestPlayerTitle).where(
+                    BestPlayerTitle.profile_id == profile_pk
+                )
+                res = await session.execute(query)
+                return res.one()[0]
+            except NoResultFound:
                 title = BestPlayerTitle(
                     best_of_the_day=0,
                     best_of_the_month=0,
                     profile_id=profile_pk,
                 )
-                session.add(title)
-
-            return title
+                try:
+                    session.add(title)
+                    await session.flush()
+                    return title
+                except IntegrityError:
+                    await session.rollback()
+                    query = select(BestPlayerTitle).where(
+                        BestPlayerTitle.profile_id == profile_pk
+                    )
+                    res = await session.execute(query)
+                    return res.one()[0]
 
     async def patch(self, profile_pk: int, **fields) -> BestPlayerTitle:
-        """Вызывается внутри другой сессии, не напрямую"""
         async with self.db.get_session() as session:
             query = (
                 update(BestPlayerTitle)
@@ -47,3 +57,21 @@ class ORMProfileTitleService(IProfileTitleService):
             )
             result = await session.execute(query)
             return result.scalar()
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    from apps.users.services.storage.base import IProfileTitleService
+    from config.containers import get_container
+
+    async def main():
+        container = get_container()
+
+        repo = container.resolve(IProfileTitleService)
+        task1 = repo.get_or_create_by_profile(6407)
+        task2 = repo.get_or_create_by_profile(6407)
+
+        await asyncio.gather(task1, task2)
+
+    asyncio.run(main())
